55dbac2bd1401bd7a541407c398cbef5932552db
Lauryn Davis <ldavis9997@yahoo.com>
2023-03-27 09:26:28 -0400

    save

---
+++ Cleaned files/Composition-of-American-Voters-by-Age-Presidential-Elections-1980–2016.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(ggtext)
+
+# DESCRIPTION:The following data visualizations are from the U.S. Census Bureau and 
+# involved data concerning the characteristics of voters in the presidential election of 2016
+
+#Original Source: https://www.census.gov/content/dam/Census/library/publications/2018/demo/P20-582.pdf
+
+# SLOW REVEAL ORDER: No slow reveal, just completly recreated. 
