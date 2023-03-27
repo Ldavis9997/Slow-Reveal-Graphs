a06e4f3c5f55a133b1191bab75022be87deb73fc
Lauryn Davis <ldavis9997@yahoo.com>
2023-03-27 09:34:59 -0400

    save

--- Cleaned files/Voting Data/Composition-of-American-Voters-by-Age-Presidential-Elections-1980–2016.R
+++ Cleaned files/Voting Data/Composition-of-American-Voters-by-Age-Presidential-Elections-1980–2016.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(ggtext)
 
-# DESCRIPTION:The following data visualizations are from the U.S. Census Bureau and 
+# DESCRIPTION:Composition of American Voters by Age: Presidential Elections, 1980–2016
+# The following data visualizations are from the U.S. Census Bureau and 
 # involved data concerning the characteristics of voters in the presidential election of 2016
 
 #Original Source: https://www.census.gov/content/dam/Census/library/publications/2018/demo/P20-582.pdf
--- Cleaned files/Voting Data/Voters-among-the-total-citizen-and-registered-voting-age-population.R
+++ Cleaned files/Voting Data/Voters-among-the-total-citizen-and-registered-voting-age-population.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(ggtext)
 
-# DESCRIPTION:The following data visualizations are from the U.S. Census Bureau and 
+# DESCRIPTION:Voters Among the Total, Citizen, and Registered Voting-Age Populations: 2016
+# The following data visualizations are from the U.S. Census Bureau and 
 # involved data concerning the characteristics of voters in the presidential election of 2016
 
 #Original Source: https://www.census.gov/content/dam/Census/library/publications/2018/demo/P20-582.pdf
